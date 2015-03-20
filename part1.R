cbcf64cc81ca99c83346280e133858bc8a82f12e
Aditya Salapaka <salapaka.aditya@gmail.com>
2015-03-20 22:32:13 +0530

    created basic script

--- part1.R
+++ part1.R
@@ -1,3 +1,20 @@
-lambda <- 40
+lambda <- 0.2
 n <- 40
-sims <- 1000
+sims <- 1:1000
+
+set.seed(850)
+
+mns <- data.frame(x = sapply(sims, function(x){
+        mean(rexp(n, lambda))
+}))
+
+
+mean(mns$x) # theoretical mean is 1/lambda
+var(mns$x) # theoretical variance is ((1/lambda)/sqrt(n))^2
+
+library(ggplot2)
+
+g <- ggplot(mns, aes(x = mns$x))
+p <- g + geom_histogram(aes(y = ..density..), binwidth = 0.4, fill = "white", color = "black") + 
+        stat_function(fun = dnorm, arg = list(mean = 5, sd = sd(mns$x)), colour = "red")
+print(p)
